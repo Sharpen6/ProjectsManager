4b698072e3550b94632359ca450281b1c75006d2
sagi baz <sharpen6@gmail.com>
2015-03-05 20:01:34 +0200

    database v1.0 uploaded

---
+++ ProjectsManager/ProjectsManager/Model/AdoHelper.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.OleDb;
+namespace ProjectsManager.Model
+{
+    class AdoHelper
+    {
+        static string connectionString = Properties.Settings.Default.DBConnection;
+        static OleDbConnection con = new OleDbConnection(connectionString);
+
+        public static DataTable ExecuteDataTable(string query)
+        {
+            try
+            {
+                con.Open();
+                OleDbCommand command = new OleDbCommand(query, con);
+                OleDbDataAdapter tableAdapter = new OleDbDataAdapter(command);
+                DataTable dt = new DataTable();
+                tableAdapter.Fill(dt);
+                return dt;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
+        public static void ExecuteNonQuery(string query)
+        {
+            try
+            {
+                con.Open();
+                OleDbCommand command = new OleDbCommand(query, con);
+                command.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+        public static object ExecuteScalar(string query)
+        {
+            try
+            {
+                con.Open();
+                OleDbCommand command = new OleDbCommand(query, con);
+                return command.ExecuteScalar();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+    }
+}
---
+++ ProjectsManager/ProjectsManager/Model/DataQueries.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectsManager.Model
+{
+    class DataQueries
+    {
+        public static DataTable GetAllDepartments()
+        {
+            try
+            {
+                string query = "Select * from Departments";
+                DataTable dt = AdoHelper.ExecuteDataTable(query);
+                return dt;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        public static void AddNewDept(int dptCode, string dptName, string dptLocation)
+        {
+            string query = "Insert into Departments values(" + dptCode + " , '" + dptName + "' , '" +
+           dptLocation + "')";
+            AdoHelper.ExecuteNonQuery(query);
+        }
+        public static void UpdateDept(int dptCode, string dptName, string dptLocation)
+        {
+            string query = "Update Departments set DepName='" + dptName + "', Location='" + dptLocation + "' where DepCode=" + dptCode;
+            AdoHelper.ExecuteNonQuery(query);
+        }
+        public static void DeleteDept(int dptCode)
+        {
+            string query = "Delete from Departments where DepCode=" + dptCode;
+            AdoHelper.ExecuteNonQuery(query);
+        }
+    }
+}
